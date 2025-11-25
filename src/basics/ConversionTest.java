36f3d7991393bd7ac15c1bfead9a6afe962001c4
pablo1312-code <jc0808888@gmail.com>
2025-11-25 12:53:29 +0100

    arrays conversion

--- src/basics/ConversionTest.java
+++ src/basics/ConversionTest.java
@@ -1,22 +1,77 @@
 package basics;
-/**
+
+import oopmodeling.CelestialBody;
+import oopmodeling.Planet;
+import oopmodeling.Star;
+
+/*
  * 
- * @author panblo
+ *
+ * 
  * 21 nov 2025
  */
-public class ConversionTest {
-	public static void main(String[] args) {
-		int num1 = 234564564;
-		byte num2 = 127;
-		float num3 = 324.4f;
-		num1 = num2;
+
+public class ConversionTest{
+	
+	public static void main(String[] args) {		
 		
+		primitiveConversion();
+		
+		//Polymorphism
+		Planet p1 = new Planet();
+		//A planet is not a star, we cannot assign a planet object to a star object
+        //Star s1 = (Star) new Planet();
+		//In the following two cases, a planet is of type CelestialBody and Object
+		CelestialBody p2 = new Planet();
+		Object p3 = new Planet();
+		Object p4 = new CelestialBody();
+		
+		Planet p5 = (Planet)p3;
+		System.out.println(p5);
+		System.out.println(p3);
+		System.out.println(p5==p3);
+		
+		Object[] planets = new Planet[20];
+		Planet[] pls = (Planet[]) planets;
+		
+		/**
+		 * 1 create an array of Object of size 20 
+		 * 2 assign each position of the array with an object of type planet using for loop
+		 * 3 convert the whole array explicitly to an array of Planet 
+		 */
+		
+		Object[] objArray = new Object[20];
+		for (int i = 0; i < 20; i++) objArray[i] = new Planet();
+
+		
+		
+		polymorphismMethod(p2); 
+		
+	}
+	
+	private static void polymorphismMethod(CelestialBody cb) {
+		
+		//an algorithm that processes an object of CelestialBody type
 		
 	}
 	
 	private static void primitiveConversion() {
 		
+		int num1 = 200;
+		byte num2 = 127;
+		float num3 = 324.4f;
+		double num4 = 34;
+		num1 = num2;
+		System.out.println(num1);
+		num1 = 129;
+		System.out.println(num1);
+		//explicit conversion from an integer to a byte number
+		num2 = (byte) num1;
+		System.out.println("El número 2 es: " + num2);
+		//Even explitly we can not and should not cast an int array to a byte array nor the contrary
+		//because the procress is complicated to do and not so useful
+		int[] nums1 = new int[23]
+		
 	}
-
 
 }
